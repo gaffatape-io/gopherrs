2e6ecb0595eadfb7719e2d37a907a31950b717d0
Daniel Petersson <dape@google.com>
2021-09-01 22:10:02 +0200

    add Wrap and a stable callstack test

--- errors.go
+++ errors.go
@@ -7,7 +7,19 @@ import (
 	"runtime"
 )
 
-var maxCallstackDepth = flag.Int("max_callstack_depth", 32, "Max stackframes to capture when an error is created")
+type framesIter interface {
+	Next() (runtime.Frame, bool)
+}
+
+type callersFramesFunc func([]uintptr) framesIter
+
+var (
+	maxCallstackDepth = flag.Int("max_callstack_depth", 32, "Max stackframes to capture when an error is created")
+
+	callersFrames callersFramesFunc = func(frames []uintptr) framesIter {
+		return runtime.CallersFrames(frames)
+	}
+)
 
 // E is the implementation of the error interface.
 type E struct {
@@ -15,6 +27,7 @@ type E struct {
 	Message string
 	FormatArgs []interface{}
 	Callstack []uintptr
+	Cause error
 }
 
 func callstack() []uintptr {
@@ -25,18 +38,29 @@ func callstack() []uintptr {
 
 // NewE returns a newly created error.
 func NewE(code Code, msg string) *E {
-	return &E{code, msg, nil, callstack()}
+	return &E{code, msg, nil, callstack(), nil}
 }
 
 // NewEf returns a new created error with formatting.
 func NewEf(code Code, msg string, args ...interface{}) *E {
-	return &E{code, msg, args, callstack()}
+	return &E{code, msg, args, callstack(), nil}
 }
 
-func writeCallstack(b *strings.Builder, callstack []uintptr) {
-	frames := runtime.CallersFrames(callstack)
+// Wrap returns an error that wraps an existing error.
+func Wrap(cause error, code Code, msg string) *E {
+	return &E{code, msg, nil, callstack(), cause}
+}
+
+// Wrapf returns an error with formatting that wraps an existing error.
+func Wrapf(cause error, code Code, msg string, args ...interface{}) *E {
+	return &E{code, msg, args, callstack(), cause}	
+}
+
+func writeCallstack(b *strings.Builder, callstack []uintptr, indent string) {
+	frames := callersFrames(callstack)
 	for {
 		frame, ok := frames.Next()
+		b.WriteString(indent)
 		b.WriteString(frame.Function)
 		b.WriteByte(' ')
 		b.WriteString(frame.File)
@@ -53,8 +77,16 @@ func writeCallstack(b *strings.Builder, callstack []uintptr) {
 // Error implements the error interface.
 func (e *E) Error() string {
 	b := &strings.Builder{}
+	e.writeError(b, "")
+	return b.String()
+}
+
+func (e *E) writeError(b *strings.Builder, indent string) {
+	b.WriteString(indent)
 	b.WriteString(e.Code.String())
+	
 	b.WriteByte(' ')
+	
 	if e.FormatArgs == nil {
 		b.WriteString(e.Message)
 	} else {
@@ -62,6 +94,20 @@ func (e *E) Error() string {
 	}
 
 	b.WriteByte('\n')
-	writeCallstack(b, e.Callstack)
-	return b.String()
+	
+	writeCallstack(b, e.Callstack, indent)
+
+	switch cause := e.Cause.(type) {
+	case *E:
+		// TODO(dape): replace the indent growth with a fixed set of string to avoid alloc.
+		cause.writeError(b, indent + "  ")
+
+	case error:
+		b.WriteString(cause.Error())
+	}
+}
+
+// Unwrap to work well with the errors package.
+func (e *E) Unwrap() error {
+	return e.Cause
 }
--- errors_test.go
+++ errors_test.go
@@ -1,71 +1,76 @@
 package gopherrs
 
 import (
-	"fmt"
 	"runtime"
 	"strings"
 	"testing"
 )
 
-// TestsEErrorCallstack checks that the callstack is correct.
-// Don't do this check in other tests!
 func TestEErrorCallstack(t *testing.T) {
-	b:= &strings.Builder{}	
-	s := make([]uintptr, 32)
-
-	msg := "abcd"
-	
-	// Keep together since this test uses the line number from
-	// the first call to figure out the line number of the second call.
-	n := runtime.Callers(1, s)
-	e := NewE(InvalidArgument, msg)
-	
-	writeCallstack(b, s[:n])
-
-	diff := &strings.Builder{}
-	got := e.Error()
-	want := "InvalidArgument " + msg + "\n" + b.String()
-
-	count := len(got)
-	if len(want) > len(got) {
-		count = len(want)
-	}
-	t.Log(len(got), len(want))
-
-	// Worst diff ever :) produces a string that can be compared against
-	// the expected result, the string have no actual meaning and can be
-	// considered a 'fingerprint' of the diff.
-	for i := 0; i < count; i++ {
-		var g, w byte
-		if i < len(got) {
-			g = got[i]
-		}
-
-		if i < len(want) {
-			w = want[i]
-		}
-
-		markDiff := func(txt string, i int) string {
-			// TODO(dape): make sure this work even if diff is at start of string.
-			return fmt.Sprintf("%s[%c]%s",txt[i-3:i], txt[i], txt[i+1:i+3])
-		}
+	const msg = "abcd"
+	const msg2 = "efgh"
+	const msgf = "%d %d %d"
+	tests := []struct {
+		err *E
+		txt string
+	} {
+		{
+			NewE(InvalidArgument, msg),
+			"InvalidArgument abcd\nfoo foo.go:1\n",
+		},
+		{
+			NewEf(InvalidArgument, msgf, 1, 2, 3),
+			"InvalidArgument 1 2 3\nfoo foo.go:1\n",
+		},
+		{
+			Wrap(NewE(InvalidArgument, msg), Unknown, msg2),
+			"Unknown efgh\nfoo foo.go:1\n  InvalidArgument abcd\n  foo foo.go:1\n",
+		},
+		{
+			Wrapf(NewE(InvalidArgument, msg), Unknown, msgf, 4, 5, 6),
+			"Unknown 4 5 6\nfoo foo.go:1\n  InvalidArgument abcd\n  foo foo.go:1\n",
+		},
 		
-		if g != w {
-			t.Logf(fmt.Sprintf("p: %d g:%q w:%q", i, markDiff(got, i), markDiff(want, i)))
-		
-			diff.WriteByte(g)
-			diff.WriteByte(w)
+	}
+
+	callersFrames = func([]uintptr) framesIter {
+		return &fakeFrames{testFrames, 0}
+	}
+	
+	for _, tc := range tests {
+		txt := tc.err.Error()
+		t.Logf("g:%q", txt)
+		t.Logf("w:%q", tc.txt)
+
+		if txt != tc.txt {
+			t.Fatal()
 		}
 	}
-
-	t.Log("diff:", diff)
-	// This check will need to be adjusted as additional imports are added to this file.
-	if diff.String() != "98" {
-		t.Fatal()
-	}
-
-	t.Log(got)
 }
+
+type fakeFrames struct {
+	frames []runtime.Frame
+	at int
+}
+
+func (f *fakeFrames) Next() (runtime.Frame, bool) {
+	if f.at == len(f.frames) {
+		return runtime.Frame{}, false
+	}
+
+	cur := f.frames[f.at]
+	f.at++
+	return cur, f.at == len(f.frames)
+}
+
+var (
+	testFrames = []runtime.Frame{
+		runtime.Frame{Function: "foo", File: "foo.go", Line: 1},
+		runtime.Frame{Function: "bar", File: "bar.go", Line: 2},
+		runtime.Frame{Function: "baz", File: "baz.go", Line: 3},
+	}
+)
+
 
 func TestNewEf(t *testing.T) {
 	e := NewEf(InvalidArgument, "foo %s", "bar")
